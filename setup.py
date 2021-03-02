39007a829e1d9bbdfdbd91ef4f122940a1c9c39a
Surui Li <lisurui6@gmail.com>
2021-03-02 11:13:40 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name="4Dsurvival",
+    use_scm_version=True,
+    author="Declan Oregan",
+    author_email="declan.oregan@lms.mrc.ac.uk",
+    description="",
+    packages=find_namespace_packages(),
+    setup_requires=["setuptools >= 40.0.0"],
+    include_package_data=True,
+)
